390e2d1bf53e80b47844f5614014bef1730adf8b
LIKITH NAIDU <likith.anumakonda@gmail.com>
2026-08-22 09:11:28 +0530

    Update Butter.java

--- 19-08-2026/Butter.java
+++ 19-08-2026/Butter.java
@@ -6,37 +6,37 @@ public class Butter {
     System.out.println(" ENTER ROWS..: ");
     int n = sc.nextInt();
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n - i; j++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
         System.out.print(" ");
       }
-      for (int j = 1; j <= i; j++) {
+      for (int j = 0; j <= i; j++) {
         System.out.print("* ");
       }
       System.out.println();
     }
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= i; j++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
         System.out.print("*");
       }
-      for (int j = 1; j <= 2 * (n - i); j++) {
+      for (int j = 0; j < 2 * (n - i - 1); j++) {
         System.out.print(" ");
       }
-      for (int j = 1; j <= i; j++) {
+      for (int j = 0; j <= i; j++) {
         System.out.print("*");
       }
       System.out.println();
     }
 
-    for (int i = n; i >= 1; i--) {
-      for (int j = 1; j <= i; j++) {
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
         System.out.print("*");
       }
-      for (int j = 1; j <= 2 * (n - i); j++) {
+      for (int j = 0; j < 2 * (n - i - 1); j++) {
         System.out.print(" ");
       }
-      for (int j = 1; j <= i; j++) {
+      for (int j = 0; j <= i; j++) {
         System.out.print("*");
       }
       System.out.println();
